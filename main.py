0bfa7b04f56db8272e9cac03d51bad4c2091a3e3
Basil Ajith <bsljth@gmail.com>
2023-05-31 23:14:15 +0530

    finished character counter function

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-def num_words(content):
-  words = content.split()
-  return len(words)
+from alph_dict import alph_dict
 
-with open("books/frankenstein.txt") as f:
-  file_text = f.read()
-  print(num_words(file_text))
+def num_words(words_arr):
+  return len(words_arr)
 
+def num_chars(words_arr, alph_dict):
+  alphs = "abcdefghijklmnopqrstuvwxyz"
+  for word in words_arr:
+    word = word.lower()
+    for char in word:
+      if char in alphs:
+        alph_dict[char] += 1
+  return alph_dict
+
+def main():
+  file_path = "books/frankenstein.txt"
+  with open(file_path) as f:
+    text_from_file = f.read()
+    words_in_file = text_from_file.split()
+
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_words(words_in_file)} words found in the document")
+    print(num_chars(words_in_file, alph_dict))
+
+main()
